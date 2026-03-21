36b4d6494ea5892a64125acb40571986060db756
Johnny Hung <jhung.op@gmail.com>
2026-03-20 19:44:53 -0700

    Add daily summary practice script

---
+++ daily_summary.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+hours = input("How many hours did you study today? ")
+topic = input("What topic did you practice today? ")
+
+print("Study Summary")
+print("Name:", name)
+print("Hours studies:", hours)
+print(name, "studied", topic, "for", hours, "hours today.")
