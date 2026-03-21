9cd6d91b4512fe0e88d29214bf92edb3840e5ffb
Johnny Hung <jhung.op@gmail.com>
2026-03-21 09:36:32 -0700

    Add Week 2 Day 1 conditional practice

---
+++ weather_advice.py
@@ -0,0 +1,8 @@
+weather = input("What is the weather like? ")
+
+if weather == "cold":
+    print("Wear a jacket.")
+elif weather == "rainy":
+    print("Bring an umbrella.")
+else:
+    print("You should be okay.")
