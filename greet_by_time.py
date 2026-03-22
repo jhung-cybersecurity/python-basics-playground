9f3a77426b78c84ddf0f4e5abb5c8da6afb571fc
Johnny Hung <jhung.op@gmail.com>
2026-03-22 12:01:27 -0700

    Add Week 2 Day 3 functions with logic

---
+++ greet_by_time.py
@@ -0,0 +1,11 @@
+def greet_by_time(time_of_day):
+    if time_of_day == "morning":
+        print("Good morning")
+    elif time_of_day == "afternoon":
+        print("Good afternoon")
+    else:
+        print("Good evening")
+
+greet_by_time("morning")
+greet_by_time("night")
+
---
+++ is_even.py
@@ -0,0 +1,8 @@
+def is_even(number):
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+is_even(10)
+is_even(11)
