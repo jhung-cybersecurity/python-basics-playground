63ddb07e85856035dd83a0e18d279860d42b52e2
Johnny Hung <jhung.op@gmail.com>
2026-04-03 20:17:10 -0700

    Complete Week 4 Day 4 Modules Practice

---
+++ week4_files/helpers.py
@@ -0,0 +1,9 @@
+def greet_user(name):
+    print("Hello,", name)
+
+def add_numbers(a,b):
+    return a + b 
+
+def subtract_numbers(a, b):
+    return a - b
+
---
+++ week4_files/use_helpers.py
@@ -0,0 +1,10 @@
+from helpers import greet_user, add_numbers, subtract_numbers
+
+greet_user("Johnny")
+
+result = add_numbers(5, 3)
+print("Sum:", result)
+
+result = subtract_numbers(9, 2)
+print("Difference:", result)
+
