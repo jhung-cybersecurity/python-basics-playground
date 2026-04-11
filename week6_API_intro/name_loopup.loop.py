22b160b0ca8062e7d95f7d7a1842d39a9f328781
Johnny Hung <jhung.op@gmail.com>
2026-04-11 14:46:05 -0700

    Name lookup loop with input validation and error handling

---
+++ week6_API_intro/name_loopup.loop.py
@@ -0,0 +1,23 @@
+import requests
+
+while True:
+    name = input("Enter a name: ")
+
+    if name == "":
+        print("You didn't enter a name. Try again.")
+    elif name == "quit":
+        print("Goodbye!")
+        break
+    else:
+        try:
+            file = requests.get(f"https://api.agify.io/?name={name}")
+            data = file.json()
+            if data["age"] is None:
+                print("There's no age associated with this name. Try again.")
+            else:
+                print("Name:", data["name"], "Age:", data["age"], "Count:", data["count"])
+        except:
+            print("Data Corrupted.")
+
+
+
