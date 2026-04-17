d5f865e50ed967203625665c71d612b94e46a93c
Johnny Hung <jhung.op@gmail.com>
2026-04-16 19:14:15 -0700

    week 7 day 2&3 chatbot with system memory & prompt persona

---
+++ week7_API_Keys/chatbot_memory.py
@@ -0,0 +1,29 @@
+import os
+from anthropic import Anthropic
+
+client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+history = []
+
+while True:
+    prompt = input("Enter your prompt: ")
+    
+
+    if prompt == "":
+        print("You didn't enter a prompt. Try again.")
+    elif prompt == "quit":
+        print("Chat ended.")
+        break
+    else:
+        try:
+            history.append({"role": "user", "content": prompt})
+            message = client.messages.create(
+                model="claude-sonnet-4-5",
+                max_tokens=1024,
+                messages=history
+            )
+            history.append({"role": "assistant", "content": message.content[0].text})
+            print(message.content[0].text)
+        except:
+            print("API failed to return result. Try again later.")
+
+
